ac4b9050cb87e6707d52a13f85ec41cc17dba70e
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-23 10:35:40 +0300

    routing, model class

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const router = require('./router');
+
 const app = express();
+
 
 /* App setup */
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 /* Server setup */
 const port = process.env.PORT || 3090;
