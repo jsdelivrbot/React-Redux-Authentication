810aab8e6b9ad20d8d4d316b43c082161f11f893
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-29 17:16:27 +0300

    welcome

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import RequireAuth from './components/auth/require_auth';
+import Welcome from './components/welcome';
 
 import reducers from './reducers';
 
@@ -20,6 +21,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={ browserHistory }>
         <Route path="/" component={ App }>
+          <IndexRoute component={ Welcome }/>
           <Route path="signin" component={ Signin }/>
           <Route path="signout" component={ Signout }/>
           <Route path="signup" component={ Signup }/>
